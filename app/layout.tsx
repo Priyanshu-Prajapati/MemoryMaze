dd7f140d64dc8fad313cf7a73a41e17152e58cc1
Priyanshu Prajapati <126185011+Priyanshu-Prajapati@users.noreply.github.com>
2025-03-20 11:35:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Memory Maze',
-  description: 'Memory Maze is a challenging puzzle game that puts your memory and navigation skills to the test. Players are briefly shown a randomly generated maze for a few seconds before it disappears. Using only their memory, they must guide their character from the start to the finish without hitting invisible walls.',
+  description: 'Memory Maze is a challenging puzzle game that puts your memory and navigation skills to the test.',
   keywords: 'Memory maze game, Memory game, Maze game, Game, Maze, Brain training, Mind puzzle, memory-based puzzle'
 }
 
