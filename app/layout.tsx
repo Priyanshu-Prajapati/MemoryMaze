e76e4cdcbd1c5d93779003b8f0c497b46382f8d0
Priyanshu Prajapati <126185011+Priyanshu-Prajapati@users.noreply.github.com>
2025-03-20 11:27:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Memory Maze',
+  description: 'Memory Maze is a challenging puzzle game that puts your memory and navigation skills to the test. Players are briefly shown a randomly generated maze for a few seconds before it disappears. Using only their memory, they must guide their character from the start to the finish without hitting invisible walls.',
 }
 
 export default function RootLayout({
