818e921a3a647d1ef09b48b6bf8abb3a73695d33
Priyanshu Prajapati <126185011+Priyanshu-Prajapati@users.noreply.github.com>
2025-03-23 17:36:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Script from "next/script"; 
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,15 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EPETNFWVQT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-EPETNFWVQT');
-</script>
       <body>{children}</body>
     </html>
   )
